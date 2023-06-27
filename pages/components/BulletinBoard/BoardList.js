e00a785f235056ed7d51f0fa91fd9cd02dd482f4
Jun <cmoonjun11@gmail.com>
2023-06-27 14:02:01 +0900

    공지사항 리스트 역순으로 수정

--- pages/components/BulletinBoard/BoardList.js
+++ pages/components/BulletinBoard/BoardList.js
@@ -3,6 +3,11 @@ import { Table, Container } from "react-bootstrap";
 import Link from "next/link";
 
 const BoardList = ({ noticeChild }) => {
+  console.log(noticeChild);
+
+  // noticeChild 배열을 역순으로 만듭니다.
+  const reversedNoticeChild = noticeChild ? [...noticeChild].reverse() : [];
+
   return (
     <>
       <h2 style={{ marginTop: "20px", marginBottom: "20px" }}>공지사항</h2>
@@ -17,29 +22,28 @@ const BoardList = ({ noticeChild }) => {
             </tr>
           </thead>
           <tbody>
-            {noticeChild &&
-              noticeChild.reverse().map((notice, index) => (
-                <tr key={notice.id}>
-                  <td>{noticeChild.length - index}</td>
-                  <td>
-                    <Link
-                      href={`https://choiii.notion.site/${notice.id.replace(
-                        /-/g,
-                        ""
-                      )}`}
-                      style={{ textDecoration: "none" }}
-                    >
-                      {notice.child_page.title}
-                    </Link>
-                  </td>
-                  <td style={{ color: "#0D6EFD" }}>운영자</td>
-                  <td>
-                    {notice.created_time.slice(0, 4)}.{" "}
-                    {notice.created_time.slice(5, 7)}.{" "}
-                    {notice.created_time.slice(8, 10)}
-                  </td>
-                </tr>
-              ))}
+            {reversedNoticeChild.map((notice, index) => (
+              <tr key={notice.id}>
+                <td>{reversedNoticeChild.length - index}</td>
+                <td>
+                  <Link
+                    href={`https://choiii.notion.site/${notice.id.replace(
+                      /-/g,
+                      ""
+                    )}`}
+                    style={{ textDecoration: "none" }}
+                  >
+                    {notice.child_page.title}
+                  </Link>
+                </td>
+                <td style={{ color: "#0D6EFD" }}>운영자</td>
+                <td>
+                  {notice.created_time.slice(0, 4)}.{" "}
+                  {notice.created_time.slice(5, 7)}.{" "}
+                  {notice.created_time.slice(8, 10)}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </Container>
